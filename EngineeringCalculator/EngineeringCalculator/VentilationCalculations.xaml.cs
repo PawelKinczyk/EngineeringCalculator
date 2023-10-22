d4bfac276351982101cb8e14deceba97479d3114
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-10-22 12:36:07 +0200

    Add picker to ventilation calculations

--- EngineeringCalculator/EngineeringCalculator/VentilationCalculations.xaml.cs
+++ EngineeringCalculator/EngineeringCalculator/VentilationCalculations.xaml.cs
@@ -4,6 +4,19 @@ public partial class VentilationCalculations : ContentPage
 {
 	public VentilationCalculations()
 	{
-		InitializeComponent();
-	}
+
+        InitializeComponent();
+
+        List<string> predefinedItems = new List<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3"
+                // Add more items as needed
+            };
+        myPicker.ItemsSource = predefinedItems;
+
+    }
+
+	
 }
