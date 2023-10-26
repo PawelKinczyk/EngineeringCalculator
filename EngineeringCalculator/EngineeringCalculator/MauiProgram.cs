c2fd9c33f8bdba87a5b08a6c48db2b45d69a7109
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-10-26 20:59:06 +0200

    Add enter frid suffix

--- EngineeringCalculator/EngineeringCalculator/MauiProgram.cs
+++ EngineeringCalculator/EngineeringCalculator/MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 
+
 namespace EngineeringCalculator;
 
 public static class MauiProgram
@@ -9,6 +10,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
