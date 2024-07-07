c30fa517ff784d531bbf8524c1cf6e075513e9ab
PawelKinczyk <pawel.kinczyk@outlook.com>
2024-07-07 14:37:37 +0200

    Copy existed page with round duct pressure loss

---
+++ EngineeringCalculator/EngineeringCalculator/RectPressureLoss.xaml.cs
@@ -0,0 +1,34 @@
+
+using EngineeringCalculator.Objects;
+using System.Collections.ObjectModel;
+
+namespace EngineeringCalculator;
+
+public partial class RectPressureLoss : ContentPage
+{
+    public ObservableCollection<Duct> DuctList { get; private set; } = new ObservableCollection<Duct>();
+    MaterialFrictionCoefficient materials = new MaterialFrictionCoefficient();
+    public RectPressureLoss()
+    {
+        InitializeComponent();
+
+        DPicker.ItemsSource = Duct.roundDuctDimensions;
+        MaterialFrictionPicker.ItemsSource = materials.GetAllFrictionCoefficientsNames();
+        AirDensityEntry.Text = "0.0000015";
+    }
+    private void GetPressureLossRound(object sender, EventArgs e)
+    {
+        DuctList.Clear();
+        if (DPicker.SelectedItem != null && AirEntry.Text != null && Double.TryParse(AirEntry.Text, out double AirFlow) == true)
+        {
+            // TODO : Clean code below
+            string materialName = (string)MaterialFrictionPicker.SelectedItem;
+            double materialRoughness = materials.GetFrictionCoefficient(materialName);
+            Duct roundDuct = new Duct { diameter = (int)DPicker.SelectedItem, crossSection = Calculations.crossSectionRoundDuct((int)DPicker.SelectedItem), materialRoughness = materialRoughness , airVolume= Double.Parse(AirEntry.Text) };
+            (roundDuct.pressureLossPerMeter, roundDuct.materialRoughness, roundDuct.reynoldsValue, roundDuct.airLiquidDensity, roundDuct.airSpeed) = Calculations.pressureLoss(roundDuct.airVolume, roundDuct.materialRoughness, roundDuct.diameter,  AirDensityEntry.Text);
+            
+            DuctList.Add(roundDuct);
+            DuctPressureLoss.ItemsSource = DuctList;
+        }
+    }
+}
